45407f6823e418206ba285c36492ddc7cc865254
EMMA SCHACTER <emmacamille@EMMAs-MacBook-Pro.local>
2016-03-10 20:14:40 -0800

    Fixing Custom and Mystery Bugs

--- DiceMainClass.py
+++ DiceMainClass.py
@@ -6,14 +6,34 @@ class DiceEdit(object):
 		self.dice_outcomes = []
 
 	def add_to_dice(self, food_list):
-		# self.dice_outcomes.extend(food)
 		for item in food_list:
-			# item_upper = self.dice_outcomes.upper()
 			if item not in self.dice_outcomes:
 				self.dice_outcomes.append(item)
+			else:
+				print "That item is already on the dice."
 		# food you pass in much be a list b/c extend
 		# what if food item already exists
 		# deal with case sensitivity
+
+	def custom_dice(self):
+		# using dice_outcomes: 
+		# while loop: while answer is not "0", add food item to list
+		# if new_item = "0": exit, dice complete.
+		# else: self.dice_outcomes.append(new_item)
+		print "You'd like to create your own dice? Cool! Here are the rules:" + "\n" + "1. Enter as many food items as you'd like." + "\n" + "2. Once you have added everything, type 0 (the number zero)." + "\n" + "3. Get unhungry!"
+
+		while len(self.dice_outcomes) < 5: # six options
+
+			new_item = raw_input("What is item you would like to add to the list?") 
+			if new_item == "0":
+				break
+				
+		 	elif (new_item in self.dice_outcomes):
+		 		# if food item exists in list, do not add
+		 		print "You've already added that item to the dice!"
+		 	else: 
+		 		# if food item does not exist, add to list
+		 		self.dice_outcomes.append(new_item)
 
 	def remove_from_dice(self, food):
 		self.dice_outcomes.remove(food)
@@ -23,7 +43,7 @@ class DiceEdit(object):
 		print "Are you ready to roll the dice?"
 		yes_no_answer = raw_input("Yes or no?")
 
-		if yes_no_answer == "NO" or yes_no_answer == "no" or yes_no_answer == "No":
+		if yes_no_answer == "NO" or yes_no_answer == "no" or yes_no_answer == "No" or yes_no_answer == "n" or yes_no_answer == "N":
 			print "Seriously, you're that indecisive. Fine."
 			#end program
 		else: 
--- dice_main.py
+++ dice_main.py
@@ -18,41 +18,54 @@ print "2 - Drunk Dice"
 print "3 - Munchies Dice"
 print "4 - Sweet Dice"
 print "5 - Custom Dice"
-print "6 - Myster Box Cooking Challenge"
+print "6 - Mystery Box Cooking Challenge"
 
 dice_choice = raw_input("Select the dice you would like to roll by typing the number on the menu:")
 dice_choice = int(dice_choice)
 
 if dice_choice == 1:
-	print "Awesome! You're sober and don't know what to eat, let me help you with that!"
-	print sober_dice_edit.random_outcome()
+	print "Being sober isn't the end of the world.  But you can't decide on what to eat! Hmmmm... sucks for you! Wait! Let me help you with that!"
+	sober_result = sober_dice_edit.random_outcome()
+	print "You're sober... so... you should eat...." + "\n" + sober_result + "\n" + "Now get eating!!!"
 	# didn't like it? Want to roll again? Removes last rolled outcome.
 
 		
 elif dice_choice == 2:
 	print "NICE! You're drunk and don't know what to eat! Let me help you with that!"
-	print drunk_dice_edit.random_outcome()
+	drunk_result = drunk_dice_edit.random_outcome()
+	print "Since you're drunk... you should definitely eat...." + "\n" + drunk_result + "\n" + "Now get eating!!! Oh, and have another drink for me, would'ya?"
+
 		
 elif dice_choice == 3:
-	print "DUDE! You're high and don't know what to eat! Let me help you with that!"
-	print munchies_dice_edit.random_outcome()
+	print "Excellent maaaaaannn! You're high and don't know what to eat! Let me help you with that!"
+	munchies_results = munchies_dice_edit.random_outcome()
+	print "While you're high... you should get down on...." + "\n" + munchies_results + "\n" + "Now get munchin'!!!"
+
 		
 elif dice_choice == 4:
 	print "SWWEEEEETT! You're looking for something sweet and don't know what to eat! Let me help you with that!"
-	print sweet_dice_edit.random_outcome()
+	sweet_results = sweet_dice_edit.random_outcome()
+	print "Your sweet tooth saver should be..." + "\n" + sweet_results + "\n" + "MMmmmmmm... so sweeet!"
 	
 elif dice_choice == 5:
 	print "So you can't decide what to eat, but you have a couple ideas! Let me help you with that!"
-	# need to add items to dice first then print random
-	# automatically save dice to file
-	# need to save name and food items
-	print custom_dice_edit.random_outcome()
+
+	print custom_dice_edit.custom_dice() # allows user to add custom outcomes
+
+	print custom_dice_edit.random_outcome() # creates random outcome
 
 else: 
 	print "Welcome to the Mystery Box Challenge! Roll the dice to find out what's in your Mystery Box!"
 
-	mystery_box_string = mystery_box.myster_box_roll()
+	mystery_box_string = mystery_box_roll()
 
-	print "In your mystery box..." + "\n" +mystery_box_string + "Now get cooking!"
+	print "Are you ready to roll the dice?"
+	yes_no_answer = raw_input("Yes or no?")
+
+	if yes_no_answer == "NO" or yes_no_answer == "no" or yes_no_answer == "No" or yes_no_answer == "n" or yes_no_answer == "N":
+		print "Seriously, you're that indecisive. Fine."
+
+	else: 
+		print "In your mystery box..." + "\n" + mystery_box_string + "Now get cooking!"
 
 
